0d41ad04f7e2ffd137e7e9d628bdc2f6f1ff76ce
P-Singto <thamiam_k@silpakorn.edu>
2024-06-20 14:54:50 +0700

    Update sol-02.py

--- เรียนรวม-python/sol-02.py
+++ เรียนรวม-python/sol-02.py
@@ -4,9 +4,7 @@ input = lambda : sys.stdin.readline().rstrip()
 current, price = [int(num) for num in input().split()]
 total = current + (price // 50)
 point = (price // 50)
-get = 0
-if total >= 10:
-    get += 1
+get = total // 10:
 print("%d" % total)
 print("%d" % point)
 print("%d" % get)
